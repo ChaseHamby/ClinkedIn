7a6e6741747693b0a5b2300ea7ccda535f08c265
Maggie Leavell <mjleavell@gmail.com>
2019-04-08 21:26:45 -0500

    added user controller

---
+++ ClinkedIn/Data/UserRepository.cs
@@ -0,0 +1,24 @@
+﻿using ClinkedIn.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinkedIn.Data
+{
+    public class UserRepository
+    {
+        static List<User> _users = new List<User>();
+
+        public User AddUser(string username, string password)
+        {
+            var newUser = new User(username, password);
+
+            newUser.Id = _users.Count + 1;
+
+            _users.Add(newUser);
+
+            return newUser;
+        }
+    }
+}
---
+++ ClinkedIn/Validators/CreateUserRequestValidator.cs
@@ -0,0 +1,17 @@
+﻿using ClinkedIn.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ClinkedIn.Validators
+{
+    public class CreateUserRequestValidator
+    {
+        public bool Validate(CreateUserRequest requestToValidate)
+        {
+            return !(string.IsNullOrEmpty(requestToValidate.Username)
+                   || string.IsNullOrEmpty(requestToValidate.Password));
+        }
+    }
+}
